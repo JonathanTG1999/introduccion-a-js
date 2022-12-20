df3b4f7c92ed3d2387593586b0f38b27d3ed38ec
Thiago <thiagogervassio@gmail.com>
2022-12-19 23:37:36 -0300

    pide datos al usuario
    Pide los datos al usuario y los muestra con un "Bienvenido" + nombre, edad, apellido.

---
+++ tareas/clase-5/clase5-tarea2.js
@@ -0,0 +1,10 @@
+const $primerNombre = document.querySelector(`#nombre-usuario`);
+const $segundoNombre = document.querySelector(`#nombre-usuario2`);
+const $apellidoUsuario = document.querySelector(`#apellido-usuario`);
+const $edadUsuario = document.querySelector(`#edad-usuario`);
+const $bontonIngreso = document.querySelector(`#ingresar`)
+
+$bontonIngreso.onclick = function(){
+    document.querySelector(`h1`).innerText = `Bienvenido ` + " " + $primerNombre.value + " " + $segundoNombre.value + " " + $apellidoUsuario.value + " de años"
+    return false
+}
