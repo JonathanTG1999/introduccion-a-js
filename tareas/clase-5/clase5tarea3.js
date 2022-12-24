f07ff2c414349563426f3279872f9e285aa548c0
Thiago <thiagogervassio@gmail.com>
2022-12-24 19:00:22 -0300

    Arreglo del html
    Cambia de lugar los nombres de los <div id=> es decir, donde estaba tiempo-clases ahora esta formulario

--- tareas/clase-5/clase5tarea3.js
+++ tareas/clase-5/clase5tarea3.js
@@ -2,20 +2,25 @@ const $botoncalcularTiempo = document.querySelector(`#calcular-tiempo-total`)
 const $botonagregarClases = document.querySelector(`#agregar-clases`)
 
 $botonagregarClases.onclick = function(){
-    const numeroClases = Number(document.querySelector(`#numero-clases`).value)
-    if (numeroClases > 0 && numeroClases <= 10)
-        agregarFormulario(numeroClases)
-        else {
-            alert("El numero de clases no existe, ingrese un numero valido del 1 al 10")
-            return false
-        }
-}
+const $botoncalcularTiempo = document.querySelector(`#calcular-tiempo-total`);
+const $botonagregarClases = document.querySelector(`#agregar-clases`);
 
-function agregarFormulario(numeroClases){
-    const $formulario = document.querySelector(`#formulario`)
-    $formulario.innerHTML = ``
-    for (i = 1; i <= numeroClases; i = i + 1){
-        $formulario.innerHTML += `<form id="clase-${i}">
+$botonagregarClases.onclick = function () {
+  const numeroClases = Number(document.querySelector(`#numero-clases`).value);
+  if (numeroClases > 0 && numeroClases <= 10) agregarFormulario(numeroClases);
+  else {
+    alert(
+      "El numero de clases no existe, ingrese un numero valido del 1 al 10"
+    );
+    return false;
+  }
+};
+
+function agregarFormulario(numeroClases) {
+  const $formulario = document.querySelector(`#formulario`);
+  $formulario.innerHTML = ``;
+  for (i = 1; i <= numeroClases; i = i + 1) {
+    $formulario.innerHTML += `<form id="clase-${i}">
     <h3 class="clase">Clase ${i}</h3>
     <label for="horas">horas</label>
     <input type="number" name="horas" id="horas-clase${i}">
@@ -24,5 +29,63 @@ function agregarFormulario(numeroClases){
     <label for="segundos">segundos</label>
     <input type="number" name="segundos" id="segundos-clase${i}">
     </form> <br/>`;
+  }
+}
+
+$botoncalcularTiempo.onclick = function () {
+  const numeroClases = Number(document.querySelector(`#numero-clases`).value);
+  const $resultadoFinal = document.querySelector(`#tiempo-total`);
+  document.querySelector(`#tiempo-clases`).classList.add(`resultado-final`);
+  let horasTotales = 0;
+  let minutosTotales = 0;
+  let segundosTotales = 0;
+  for (i = 1; i <= numeroClases; i = i + 1) {
+    horasTotales = horasTotales + Number(document.querySelector(`#horas-clase${i}`).value);
+    minutosTotales =
+      minutosTotales +
+      Number(document.querySelector(`#minutos-clase${i}`).value);
+    segundosTotales =
+      segundosTotales +
+      Number(document.querySelector(`#segundos-clase${i}`).value);
+  }
+
+  $resultadoFinal.innerText = calculoTotal(
+    horasTotales,
+    minutosTotales,
+    segundosTotales
+  );
+};
+
+function calculoTotal (horas,minutos,segundos){
+    const tiempoTotalEnMinySeg = 60
+    let segundosFinales = 0
+    let minutosFinales = 0
+    let horasFinales = 0
+    if (segundos < tiempoTotalEnMinySeg){
+        segundosFinales = segundosFinales + segundos
+
+    } else if (segundos % tiempoTotalEnMinySeg === 0) {
+        minutosFinales = minutosFinales + (segundos/tiempoTotalEnMinySeg)
+    } else {
+        segundosFinales = segundosFinales + (segundos % tiempoTotalEnMinySeg)
+        minutosFinales = minutos + ((segundos - segundosFinales) / tiempoTotalEnMinySeg)
     }
+
+    if (minutos < tiempoTotalEnMinySeg){
+        minutosFinales = minutos + minutosFinales
+    } else if (minutos % tiempoTotalEnMinySeg === 0) {
+        horasFinales = horasFinales + (minutos / tiempoTotalEnMinySeg)
+    } else {
+        minutosFinales = minutosFinales + (minutos % tiempoTotalEnMinySeg)
+        horasFinales = horasFinales + ((minutos - (minutos % tiempoTotalEnMinySeg)) / tiempoTotalEnMinySeg)
+    }
+
+    if (minutosFinales > tiempoTotalEnMinySeg){
+        minutosFinales = minutosFinales + (minutosFinales % tiempoTotalEnMinySeg)
+        horasFinales = horasFinales + ((minutosFinales - (minutosFinales % tiempoTotalEnMinySeg)) / tiempoTotalEnMinySeg)
+    }
+
+    horasFinales = horasFinales + horas
+
+    return `El tiempo total de los cursos es ${horasFinales} horas, ${minutosFinales} minutos, ${segundosFinales} segundos`
 }
