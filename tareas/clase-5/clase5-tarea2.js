a35514e09897017d27c582be5cf88098e2eff93c
Thiago <thiagogervassio@gmail.com>
2023-01-25 13:14:43 -0300

    Agrega funcion
    Agrega la funcion validarNombresIngresados

--- tareas/clase-5/clase5-tarea2.js
+++ tareas/clase-5/clase5-tarea2.js
@@ -1,10 +1,27 @@
-const $primerNombre = document.querySelector("#nombre-usuario").value;
-const $segundoNombre = document.querySelector("#nombre-usuario2").value;
-const $apellidoUsuario = document.querySelector("#apellido-usuario").value;
-const $edadUsuario = document.querySelector("#edad-usuario").value;
-const $bontonIngreso = document.querySelector("#ingresar").value;
+const $primerNombre = document.querySelector("#nombre-usuario");
+const $segundoNombre = document.querySelector("#nombre-usuario2");
+const $apellidoUsuario = document.querySelector("#apellido-usuario");
+const $edadUsuario = document.querySelector("#edad-usuario");
+const $bontonIngreso = document.querySelector("#ingresar");
 
-$bontonIngreso.onclick = function(){
-    document.querySelector(`h1`).innerText = `Bienvenido ${$primerNombre} + ${$segundoNombre} + ${$apellidoUsuario} + de ${$edadUsuario} años`
-    return false
+$bontonIngreso.onclick = function (event) {
+  document.querySelector(
+    `h1`
+  ).innerText = `Bienvenido ${$primerNombre.value} + ${$segundoNombre.value} + ${$apellidoUsuario.value} + de ${$edadUsuario.value} años`;
+  event.preventDefault();
+};
+
+function validarNombresIngresados(
+  $primerNombre,
+  $segundoNombre,
+  $apellidoUsuario,
+  $edadUsuario
+) {
+  if ($primerNombre === "" || $apellidoUsuario === "" || $edadUsuario === "") {
+    return 0;
+  } else if ($segundoNombre === "") {
+    return 1;
+  } else {
+    return 2;
+  }
 }
